5a97c409bdf98203de9614096dcf1fbba2a619bd
Priy Ranjan Raj <108989422+16PriyRanjan@users.noreply.github.com>
2023-08-28 23:30:08 +0530

    Add files via upload

---
+++ pattern4.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int n;
+cin>>n;int i=1;
+while(i<=n){
+	int j=1;
+	while(j<=i){
+		cout<<j<<" ";
+		j++;
+	}
+	cout<<endl;
+	i=i+1;
+	
+}
+return 0;
+}
+/*
+1 
+1 2 
+1 2 3*/
+

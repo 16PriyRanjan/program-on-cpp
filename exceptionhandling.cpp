33e1f201192e5c914b94f3ec730571556962e7cd
Priy Ranjan Raj <108989422+16PriyRanjan@users.noreply.github.com>
2023-09-08 23:46:55 +0530

    Add files via upload

---
+++ exceptionhandling.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+double division(int a, int b) {
+   if( b == 0 ) {
+      throw "Division by zero condition!";
+   }
+   return (a/b);
+}
+
+int main () {
+   int x,y;
+   cout<<"Enter dividend";
+   cin>>x;
+   cout<<"Enter divisor";
+   cin>>y;
+   
+   double z = 0;
+ 
+   try {
+      z = division(x, y);
+      cout << z << endl;
+   } catch (const char* msg) {
+     cerr << msg << endl;
+   }
+
+   return 0;
+}
+//Because we are raising an

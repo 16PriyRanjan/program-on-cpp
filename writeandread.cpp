32a6fb870cdf3429f724d576903479dfb5c8f712
Priy Ranjan Raj <108989422+16PriyRanjan@users.noreply.github.com>
2023-09-12 23:33:49 +0530

    Add files via upload

---
+++ writeandread.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+int main()
+{
+	char data[100];
+	ofstream outfile;
+	outfile.open("afile.dat");
+	cout<<"Writing to the file"<<endl;
+	cout<<"Enter your name:";
+	cin.getline(data,100);
+	outfile << data << endl;
+	cout<<"Enter your age :";
+	cin>>data;
+	cin.ignore();
+	outfile<<data<<endl;
+	outfile.close();
+	ifstream infile;
+	infile.open("afile.dat");
+	cout<<"Reading from the file"<<endl;
+	infile>>data;
+	cout<<"Your name "<<data<<endl;
+	infile>>data;
+	cout<<"Your age "<<data<<endl;
+	infile.close();
+	return 0;
+}
